1eca0289e044948736b6e202c19fc96bddbac11d
SchoeCR <159347003+SchoeCR@users.noreply.github.com>
2024-09-21 11:10:48 +1000

    Added second unit test.

--- BlazorApp/MovieApi.Tests/MovieTests.cs
+++ BlazorApp/MovieApi.Tests/MovieTests.cs
@@ -3,7 +3,7 @@ namespace MovieApi.Tests;
 public class MovieTests
 {
     [Fact]
-    public void BuildPrintableMovieTitleWithYearLessThanZero()
+    public void BuildPrintableMovieTitleWithYearGreaterThanZero()
     {
         // Arrange
         var movie = new Movie
@@ -14,5 +14,19 @@ public class MovieTests
 
         // Assert
         Assert.Equal("Title:My Test Movie Year:2002", printableMovieTitle);
+    }
+
+    [Fact]
+    public void BuildPrintableMovieTitleWithYearLessThanZero()
+    {
+        // Arrange
+        var movie = new Movie
+        { Id = 1, Title = "My Test Movie", PosterUri = "https://imbd.com/graphic/png/10002.png", Year = -5 };
+
+        // Act
+        var printableMovieTitle = movie.BuildPrintableMovieTitle();
+
+        // Assert
+        Assert.Equal("Title:My Test Movie", printableMovieTitle);
     }
 }
