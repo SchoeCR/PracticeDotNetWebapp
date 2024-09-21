687efc6da3bc4d3c0ff003a7debc84b7685cf13a
Rob Pearson <rob.pearson@me.com>
2024-09-21 14:52:58 +1000

    Disconnected Front end And Movie API

---
+++ BlazorApp/BlazorApp/MovieDto.cs
@@ -0,0 +1,9 @@
+namespace BlazorApp;
+
+public class MovieDto
+{
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Year { get; set; }
+    public string PosterUri { get; set; }
+}
--- BlazorApp/MovieApi/Data/SeedData.cs
+++ BlazorApp/MovieApi/Data/SeedData.cs
@@ -10,7 +10,6 @@ public static class SeedData
 
         context.Add(new Movie
             {
-                Id = 1,
                 Title = "Forrest Gump",
                 Year = 2020,
                 PosterUri = "https://en.wikipedia.org/wiki/Forrest_Gump#/media/File:Forrest_Gump_poster.jpg"
@@ -18,7 +17,6 @@ public static class SeedData
         
         context.Add(new Movie
         {
-            Id = 2,
             Title = "Dodge Ball",
             Year = 2016,
             PosterUri = "https://en.wikipedia.org/wiki/Dodgeball:_A_True_Underdog_Story#/media/File:Movie_poster_Dodgeball_A_True_Underdog_Story.jpg"
